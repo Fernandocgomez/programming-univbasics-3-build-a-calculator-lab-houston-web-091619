60cdb4fe7dec07924d49d894fe94d43963205589
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-09-04 18:00:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,24 +1,28 @@
 def addition(num1, num2)
   result = num1 + num2
-  print result
+  p result
 end
 
 def subtraction(num1, num2)
-
+  result = num1 - num2
+  p result
 end
 
 def division(num1, num2)
-
+  result = num1 / num2
+  p result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  p result
 end
 
 def modulo(num1, num2)
-
+  result = num1 % num2
+  p result
 end
 
 def square_root(num)
-
+  result = 
 end
